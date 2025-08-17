1172983b0c1090cd68165de94f9a020d5a769d0f
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-08-17 12:24:13 +0200

    Strategy 2 that actually works along side mine that heavily underestimated. Have not deleted min yet

--- Codefile.R
+++ Codefile.R
@@ -98,7 +98,7 @@ study_level_data = rbind(fdata,fdata2)
 #the algorithm will work as long it is supplied with data that is not binned and weighted but is validated binning and weighting happen within the function.
 Study_level_call_density = function(examples,num_beta_samples=1000,beta_prior=0.1){
   #examples = Data2
-  examples = unlist(fdata)
+  examples = unlist(examples)
   df <- as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE) #This will be the one that i check my bin and bin_weight generation  
   examples = as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE)
   examples = rename(examples,binNumber=bin, outcome = is_pos)
@@ -161,7 +161,9 @@ Study_level_call_density = function(examples,num_beta_samples=1000,beta_prior=0.
               Samples = q_betas, 
               beta_parameters = shape.parameters,
               Study_level_weights = bin_weights,
-              densities = density_matrix))
+              densities = density_matrix,
+              negative_counts = bin_negative,
+              positive_counts = bin_number))
 }
 
 #Implementation of call density  
@@ -251,89 +253,103 @@ cat("95% Confidence interval: [",round(confidence_interval,4),"]",
     "\nstandard deviation: ",round(Sd_bootstrap_samples,4))
 
 #---------Strategy 2------------------------------------------------------------
-#P(b|+)
 Strat2_call_density = function(examples, num_beta_samples = 1000,beta_prior = 0.1){
-  examples = unlist(examples)
-  df <- as.data.frame(do.call(rbind, fdata),
-                      stringsAsFactors = FALSE) #This will be the one that i check my bin and bin_weight generation  
   
+  #restructuring of data to make it usable
+  examples = unlist(examples)
+  df = as.data.frame(do.call(rbind, fdata),stringsAsFactors = FALSE) 
   examples = as.data.frame(do.call(rbind, fdata),stringsAsFactors = FALSE)
   examples = rename(examples,binNumber=bin, outcome = is_pos)
   
-  #Positive/negative bin counts at the site level
-  bin_number  = as.matrix(distinct(examples,binNumber)) 
-  bin_number = as.matrix(sort(as.numeric(as.vector(bin_number[,1]))))
-  bin_weights = matrix(NA, nrow = nrow(bin_number),ncol=1)
-  bin_positive = matrix(NA,nrow = nrow(bin_number), ncol = 2)
-  bin_negative = matrix(NA,nrow = nrow(bin_number), ncol = 2)
+  #P_s(b) site level probability of bin
+  #Probability of bin given positive identification. Collected from study level
+  Prob.bin.given.pos = out$densities/sum(out$densities)
   
-  #Bin weights site level
-  for(i in 1:nrow(bin_number)){
-    bin_weights[i,1]=nrow(filter(examples,binNumber==i))/nrow(examples)
-  }
+  #Site level density taken from strategy 1
+  Strat1.Site.densities = out1$bin_densities
   
-  #Putting in the counts of each bin (positive/negative)
-  for(i in 1:nrow(bin_number)){
-    examples2 = filter(examples,binNumber==i)
-    bin_positive[i,2] = as.numeric(count(filter(examples2,outcome==1)))
-    bin_negative[i,2] = as.numeric(count(filter(examples2,outcome==-1)))
-  }
-  bin_positive[,1]=bin_number ; bin_negative[,1] = bin_number
+  #Study level Probability of a bin given negative count
+  Prob.bin.given.neg = matrix(out$negative_counts[,2]/sum(out$negative_counts[,2]),
+                              ncol = 1)
+  
+  #1 - Site level density taken from strategy 1
+  One.less.site.density = 1-Strat1.Site.densities
+  
+  #Probability of site level bin 
+  Prob.site.level.bin = Prob.bin.given.pos*Strat1.Site.densities + Prob.bin.given.neg*One.less.site.density
   
   
-  #Density  calculations
-  #Calculation of P(+|b)P(b)/P(+)
-  b_given_pos = out$densities/out$Density_Estimate
+  #Q_s(b) for arbitrary mixture of positives and negatives
+  seq.of.q = seq(0,1,length.out = 100)
   
-  #Calculation of P(-|b)P(b)/P(-)
-  # for the neg i have to get into each bin and find the proportion of negative examples
-  neg_densities = matrix(NA,nrow = nrow(bin_number),
-                         ncol =1)
+  Qs = list()
   
-  for( i in 1:nrow(bin_number)){
-    neg_densities[i,1] = rbeta(1,out$beta_parameters[i,2] + beta_prior,
-                              out$beta_parameters[i,1] + beta_prior)*bin_weights[i,1]
+  for( i in 1:length(seq.of.q)){
+    qs = Prob.bin.given.pos*seq.of.q[i] + Prob.bin.given.neg*(1-seq.of.q[i])
+    Qs[[length(Qs) + 1]] = qs
   }
   
   
-  b_given_neg = neg_densities / sum(neg_densities)
+  #KL divergence
+  min.and.q = matrix(NA,nrow = length(seq.of.q),ncol=2)
   
+  for(i in 1:length(seq.of.q)){
+  min.and.q[i,1] = sum(Prob.site.level.bin*(log(Prob.site.level.bin)-log(Qs[[i]])))
+  min.and.q[i,2] = seq.of.q[i]
   
-  #Density  
-  
-  # site density estimation P_s(+)
-  shape.parameters_site
-  density_matrix = matrix(NA,nrow = nrow(bin_number),ncol = 1)
-  for(i in 1:nrow(shape.parameters_site)){
-    density_matrix[i,1] = rbeta(1,out$beta_parameters[i,1]+beta_prior,
-                                out$beta_parameters[i,2]+beta_prior)*bin_weights[i,1]
   }
   
- # P_s(+)
-  prob_b_s = b_given_pos*density_matrix + b_given_neg * (1- density_matrix)
-  
-  
-  #KL divergence with Q_q(b)
-  q_seq = seq(0.0001,0.9999,length.out = 100)
-  
-  Q_q_b = b_given_pos * q_seq[1] + b_given_neg * (1-q_seq[1])
-  KL_div.old = sum(prob_b_s*(log(prob_b_s)-log(Q_q_b)))
-  Site_density = NA
-  
-  for (i in 2:length(q_seq)){
-    Q_q_b = b_given_pos*q_seq[i] + b_given_neg * (1-q_seq[i])
-    KL_div = sum(prob_b_s*(log(prob_b_s)-log(Q_q_b)))
-    if (KL_div <= KL_div.old){
-      KL_div.old = KL_div
-      Site_density = q_seq[i]
-    }
-  }
-  
-  return(Density_Estimate = Site_density)
+  seq.of.q[which.min(min.and.q[,1])]
 }
 
 Strat2_call_density(fdata,1000,0.1)
 
+
+
+# Strategy 2 that works but do not know wny mine is not working
+Strat2_call_density <- function(site_examples, study_fit = out, eps = 1e-12) {
+  # 1) SITE: empirical P_s(b)
+  site_examples = fdata
+  site_df = as.data.frame(do.call(rbind, site_examples),
+                          stringsAsFactors = FALSE) |> 
+    dplyr::rename(binNumber = bin)
+  B = nrow(study_fit$beta_parameters)
+  Ps_b = sapply(1:B, function(b) mean(as.numeric(site_df$binNumber) == b)) #Site bin weights
+  Ps_b[Ps_b < eps] = eps # to avoid 0s when calculating things like log
+  Ps_b = Ps_b / sum(Ps_b) #proportioning to it to make sure it sums to 1 after some may be set to 0
+  
+  # 2) STUDY: expected P(b|+) and P(b|-)
+  alpha = study_fit$beta_parameters[,1]             # = k_pos + c
+  beta = study_fit$beta_parameters[,2]              # = k_neg + c
+  E_pos_given_b <- alpha / (alpha + beta)           # E[P(+|b)]
+  P_b = as.vector(study_fit$Study_level_weights)    # study P(b)
+  P_b = pmax(P_b, eps)
+  P_b = P_b / sum(P_b)  #Also to make sure in case < eps came true
+  
+  P_pos = sum(E_pos_given_b * P_b)
+  P_b_pos = (E_pos_given_b * P_b) / P_pos              # P(+|b)P(b)/P(+)
+  P_b_neg = ((1 - E_pos_given_b) * P_b) / (1 - P_pos)  # P(-|b)P(b)/(1-P(+))
+  
+  P_b_pos = pmax(P_b_pos, eps)
+  P_b_pos = P_b_pos / sum(P_b_pos) #in case < eps was true
+  P_b_neg = pmax(P_b_neg, eps)
+  P_b_neg = P_b_neg / sum(P_b_neg) #in case < eps was true
+  
+  
+  
+  # 3) Grid search q in (0,1) to minimize KL( P_s(b) || q P(b|+) + (1-q) P(b|-) )
+  q_seq = seq(0.001, 0.999, length.out = 1001)   # avoid exact 0/1
+  KL_vals = sapply(q_seq, function(q) {
+    Q = q * P_b_pos + (1 - q) * P_b_neg
+    Q = pmax(Q, eps)
+    Q = Q / sum(Q) # in case < eps was true
+    sum(Ps_b * (log(Ps_b) - log(Q)))
+  })
+  q_seq[which.min(KL_vals)]
+}
+
+
+Strat2_call_density(fdata,study_fit = out,eps = 1e-12)
 
 
 #-------------------------Strategy 3 -------------------------------------------
@@ -349,6 +365,11 @@ Strat3_call_density(Strat1_call_density(fdata,
                                         10000,
                                         0.1))
 
+#it works need to figure out why and how
+Strat3_call_density(Strat1_call_density(fdata,
+                                        10000,
+                                        0.1)$Density_Estimate,
+                    Strat2_call_density(fdata,study_fit = out,eps = 1e-12))
 
 
 
