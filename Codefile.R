1fd10d66faefe56b3d428cf5bb7487e513b5a03b
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-08-26 19:36:08 +0200

    Revised working code. KL divergence fixed

--- Codefile.R
+++ Codefile.R
@@ -24,10 +24,8 @@ new_validation_example = function(filename,
 
 #Generating the fake data
 generate_fake_validation_data = function(n = 100, bins = 5) {
-
-  scores = runif(n,min = 0,max = 1)
-  #scores = rexp(1,rate =3)
-  #scores = scores[scores<=1]
+  scores = rexp(n,rate =6)
+  scores = scores/max(scores) # to make sure it <= 1
   quantile_bounds = seq(0,
                         1,
                         length.out = bins + 1)
@@ -45,7 +43,7 @@ generate_fake_validation_data = function(n = 100, bins = 5) {
       filename = paste0("file_", sample(1:10, 1), ".wav"),
       timestamp_offset = runif(1, 0, 60),
       score = scores[i],
-      is_pos = sample(c(1, -1, 0), 1, prob = c(0.55, 0.35, 0.1)),
+      is_pos = sample(c(1, -1, 0), 1, prob = c(0.20, 0.7, 0.1)),
       bin = bin_indices[i],
       bin_weight = bin_weights_vec[i]
     )
@@ -54,11 +52,9 @@ generate_fake_validation_data = function(n = 100, bins = 5) {
   return(examples)
 }
 
-generate_fake_validation_data2 <- function(n = 100, bins = 5) {
-  
-  scores = runif(n,min = 0,max = 1)
-  #scores = rexp(1,rate =4)
-  #scores = scores[scores<=1]
+generate_fake_validation_data2 = function(n = 100, bins = 5) {
+  scores = rexp(n,rate = 6)
+  scores = scores/max(scores) # to makes ure it is <= 1
   quantile_bounds = seq(0,
                         1,
                         length.out = bins + 1)
@@ -76,7 +72,7 @@ generate_fake_validation_data2 <- function(n = 100, bins = 5) {
       filename = paste0("file_", sample(1:10, 1), ".wav"),
       timestamp_offset = runif(1, 0, 60),
       score = scores[i],
-      is_pos = sample(c(1, -1, 0), 1, prob = c(0.55, 0.35, 0.1)),
+      is_pos = sample(c(1, -1, 0), 1, prob = c(0.20, 0.35, 0.45)),
       bin = bin_indices[i],
       bin_weight = bin_weights_vec[i]
     )
@@ -86,24 +82,29 @@ generate_fake_validation_data2 <- function(n = 100, bins = 5) {
 }
 
 #debug(generate_fake_validation_data)
-fdata = generate_fake_validation_data(n = 2000,
+fdata = generate_fake_validation_data(n = 1000,
                                       bins= 5)
-fdata2 = generate_fake_validation_data2(n = 2000,
+fdata2 = generate_fake_validation_data2(n = 1000,
                                        bins= 5)
 
 study_level_data = rbind(fdata,fdata2)
 
 
 # -------Chunk 2 - Call density estimation-------------------------------------
-#the algorithm will work as long it is supplied with data that is not binned and weighted but is validated binning and weighting happen within the function.
+#binning and weighting are calculalted within the function
 Study_level_call_density = function(examples,num_beta_samples=1000,beta_prior=0.1){
-  #examples = Data2
-  examples = unlist(examples)
-  df <- as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE) #This will be the one that i check my bin and bin_weight generation  
-  examples = as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE)
-  examples = rename(examples,binNumber=bin, outcome = is_pos)
-
   
+  #loading in of examples data and converting it to usable format
+  examples = unlist(examples)
+  df = as.data.frame(do.call(rbind, study_level_data), stringsAsFactors = FALSE)  #This will be the one that i check my
+  logit.scores = sapply(unlist(df$score), function(b) log(b)-log(1-b))            #generating logit scores
+  df$score = logit.scores                                                         #turning scores to logit scores
+  examples = as.data.frame(do.call(rbind, study_level_data), 
+                           stringsAsFactors = FALSE)                              #Conversion to data-frame
+  examples = rename(examples,binNumber=bin, outcome = is_pos)                     #renaming of columns
+
+ 
+  #Binning and bin weighting 
   #Positive/negative bin counts
   bin_number  = as.matrix(distinct(examples,binNumber)) 
   bin_number = as.matrix(sort(as.numeric(as.vector(bin_number[,1]))))
@@ -140,23 +141,29 @@ Study_level_call_density = function(examples,num_beta_samples=1000,beta_prior=0.
   shape.parameters
   density_matrix = matrix(NA,nrow = nrow(bin_number),ncol = 1)
   for(i in 1:nrow(shape.parameters)){
-    density_matrix[i,1] = rbeta(1,shape.parameters[i,1]+beta_prior,
-                             shape.parameters[i,2]+beta_prior)*bin_weights[i,1]
+    density_matrix[i,1] = rbeta(1,shape.parameters[i,1]+ beta_prior,
+                             shape.parameters[i,2]+  beta_prior)*bin_weights[i,1]
   }
   
   density = sum(density_matrix)
     
 #Bootstrapping: To get distribution of density estimate
-  q_betas=c()
   NoBetaSamples = 10000
-  for(i in 1:NoBetaSamples){
-    for(j in 1:nrow(bin_number)){
-      rvalue = 0
-      rvalue = rvalue + rbeta(1,shape.parameters[j,1],
-                              shape.parameters[j,2])
-    }
-    q_betas=c(q_betas,rvalue)
+  q_betas = numeric(NoBetaSamples)
+  
+  for (i in seq_len(NoBetaSamples)) {
+    # draw a bin-level theta_b from the same Beta as the estimator
+    betas = rbeta(
+      n = nrow(shape.parameters),
+      shape1 = shape.parameters[,1] + beta_prior,  # match point estimator
+      shape2 = shape.parameters[,2] + beta_prior
+    )
+    # weighted sum across bins (MATCH the estimator)
+    q_betas[i] <- sum(betas * bin_weights[,1])
   }
+  
+  
+  
   return(list(Density_Estimate = density,
               Samples = q_betas, 
               beta_parameters = shape.parameters,
@@ -187,13 +194,17 @@ cat("95% Confidence interval: [",round(confidence_interval,4),"]",
 #Strategy 1
 #every thing is the same except that now in this case, we are looking to substitute into formulas the matter of having
 #sum(P(+|b)*P_s(b))
-Strat1_call_density = function(examples, num_beta_samples=1000, beta_prior=0.1){
-  
-  examples = unlist(examples)
-  df <- as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE) #This will be the one that i check my bin and bin_weight generation  
-  examples = as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE)
-  examples = rename(examples,binNumber=bin, outcome = is_pos)
+Strat1_call_density <- function(examples, num_beta_samples=1000, beta_prior=0.1){
 
+  #loading in of examples data and converting it to usable format
+  examples = unlist(examples)
+  df = as.data.frame(do.call(rbind, fdata), stringsAsFactors = FALSE)  #This will be the one that i check my
+  logit.scores = sapply(unlist(df$score), function(b) log(b)-log(1-b))            #generating logit scores
+  df$score = logit.scores                                                         #turning scores to logit scores
+  examples = as.data.frame(do.call(rbind, fdata), 
+                           stringsAsFactors = FALSE)                              #Conversion to data-frame
+  examples = rename(examples,binNumber=bin, outcome = is_pos)                     #renaming of columns
+  
   
   #Positive/negative bin counts
   bin_number  = as.matrix(distinct(examples,binNumber)) 
@@ -219,18 +230,23 @@ Strat1_call_density = function(examples, num_beta_samples=1000, beta_prior=0.1){
   }
   density = sum(density_matrix)
 
-    
-#Bootstrapping: To get distribution of density estimate
-  q_betas=c()
+  
+  #Bootstrapping: To get distribution of density estimate
   NoBetaSamples = 10000
-  for(i in 1:NoBetaSamples){
-    for(j in 1:nrow(bin_number)){
-      rvalue = 0
-      rvalue = rvalue + rbeta(1,out$beta_parameters[j,1],
-                              out$beta_parameters[j,2])
-    }
-    q_betas=c(q_betas,rvalue)
+  q_betas = numeric(NoBetaSamples)
+  
+  for (i in seq_len(NoBetaSamples)) {
+    # draw a bin-level theta_b from the same Beta as the estimator
+    betas = rbeta(
+      n = nrow(out$beta_parameters),
+      shape1 = out$beta_parameters[,1] + beta_prior,  # match point estimator
+      shape2 = out$beta_parameters[,2] + beta_prior
+    )
+    # weighted sum across bins (MATCH the estimator)
+    q_betas[i] <- sum(betas * bin_weights[,1])
   }
+  
+  
   return(list(Density_Estimate = density,
               Samples = q_betas,
               bin_densities = density_matrix))
@@ -253,21 +269,45 @@ cat("95% Confidence interval: [",round(confidence_interval,4),"]",
     "\nstandard deviation: ",round(Sd_bootstrap_samples,4))
 
 #---------Strategy 2------------------------------------------------------------
-Strat2_call_density <- function(site_examples, study_fit = out, eps = 1e-12) {
-  # 1) SITE: empirical P_s(b)
+Strat2_call_density = function(site_examples, study_fit = out, eps = 1e-12,beta_prior= 0.1) {
+  #Site level:  empirical measurements
   #site_examples = fdata
   site_df = as.data.frame(do.call(rbind, site_examples),
                           stringsAsFactors = FALSE) |> 
     dplyr::rename(binNumber = bin)
+  logit.scores = sapply(unlist(site_df$score), function(b) log(b)-log(1-b))            #generating logit scores
+  site_df$score = logit.scores 
+
   B = nrow(study_fit$beta_parameters)
-  Ps_b = sapply(1:B, function(b) mean(as.numeric(site_df$binNumber) == b)) #Site bin weights
-  Ps_b[Ps_b < eps] = eps # to avoid 0s when calculating things like log
-  Ps_b = Ps_b / sum(Ps_b) #proportioning to it to make sure it sums to 1 after some may be set to 0
+  site_bin_weight = sapply(1:B, function(b) mean(as.numeric(site_df$binNumber) == b)) #Site bin weights
+  site_bin_weight[site_bin_weight < eps] = eps # to avoid 0s when calculating things like log
+  site_bin_weight = site_bin_weight / sum(site_bin_weight)#proportioning to it to make sure it sums to 1 after some may be set to 0
   
-  # 2) STUDY: expected P(b|+) and P(b|-)
+  site_bin_positive = matrix(NA,ncol = 2, nrow = B)
+  site_bin_negative = matrix(NA,ncol = 2, nrow = B)
+  
+  for(i in 1:B){
+    dataframe = filter(site_df,binNumber==i)
+    site_bin_positive[i,2] = as.numeric(count(filter(dataframe, dataframe$is_pos==1)))
+    site_bin_negative[i,2] = as.numeric(count(filter(dataframe, dataframe$is_pos==-1)))
+    
+  }
+  site_bin_negative[,1] = c(seq(1,B))
+  site_bin_positive[,1] = c(seq(1,B))
+  
+    #site level density
+  site_level_densities = matrix(NA,ncol=1,nrow = B)
+  for(i in 1:B){
+    site_level_densities[i,1] = rbeta(1,
+                                      site_bin_positive[i,2]+beta_prior,
+                                      site_bin_negative[i,2]+beta_prior)*site_bin_weight[i]
+  }
+  site_level_density = sum(site_level_densities)
+  
+  #Study level: expected P(b|+) and P(b|-)
   alpha = study_fit$beta_parameters[,1]             # = k_pos + c
   beta = study_fit$beta_parameters[,2]              # = k_neg + c
-  E_pos_given_b <- alpha / (alpha + beta)           # E[P(+|b)]
+  E_pos_given_b = alpha / (alpha + beta)           # E[P(+|b)]
   P_b = as.vector(study_fit$Study_level_weights)    # study P(b)
   P_b = pmax(P_b, eps)
   P_b = P_b / sum(P_b)  #Also to make sure in case < eps came true
@@ -282,22 +322,27 @@ Strat2_call_density <- function(site_examples, study_fit = out, eps = 1e-12) {
   P_b_neg = P_b_neg / sum(P_b_neg) #in case < eps was true
   
   
+  #Grid search q in (0,1) to minimize KL( P_s(b) || q P(b|+) + (1-q) P(b|-) )
+  Ps_b = P_b_pos*site_level_density + P_b_neg*(1-site_level_density)
   
-  # 3) Grid search q in (0,1) to minimize KL( P_s(b) || q P(b|+) + (1-q) P(b|-) )
-  q_seq = seq(0.001, 0.999, length.out = 1001)   # avoid exact 0/1
+  q_seq = seq(0.001, 0.999, length.out = 2001)   # avoid exact 0/1
   KL_vals = sapply(q_seq, function(q) {
     Q = q * P_b_pos + (1 - q) * P_b_neg
     Q = pmax(Q, eps)
     Q = Q / sum(Q) # in case < eps was true
     sum(Ps_b * (log(Ps_b) - log(Q)))
   })
-  q_seq[which.min(KL_vals)]
+  return(Stat2_density_estimate = q_seq[which.min(KL_vals)])
 }
 
-bootstrap_strat2_site <- function(site_examples, study_fit,
+bootstrap_strat2_site = function(site_examples, study_fit,
                                   Bdraws = 2000, 
                                   conf = 0.95, 
                                   eps = 1e-12) {
+  
+  #site_examples = fdata
+  #study_fit = out
+  #eps = 1e-12
   
   n = length(site_examples)
   
@@ -307,7 +352,7 @@ bootstrap_strat2_site <- function(site_examples, study_fit,
   
  
   
-  boots = numeric(Bdraws)
+  boots = Bdraws
   for (b in seq_len(Bdraws)) {
     idx = sample.int(n, n, replace = TRUE)
     boots[b] = Strat2_call_density(site_examples[idx],
@@ -329,7 +374,10 @@ out2 = bootstrap_strat2_site(fdata,out,Bdraws = 2000, conf = 0.95, eps = 1e-12)
 
 hist(out2$samples)
 out2$conf_int
+out2$point
 out$Density_Estimate
+
+
 
 #-------------------------Strategy 3 -------------------------------------------
 Strat3_call_density = function(density1, density2){
@@ -399,7 +447,7 @@ bootstrap_strat3_site = function(site_examples,
 
 out3 = bootstrap_strat3_site(fdata,out,Bdraws = 2000, conf = 0.95, eps = 1e-12)
 hist(out3$samples)
-
+out3$point
 
 
 
